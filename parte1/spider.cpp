0c702c17b90ddaf979bb953dc4fae8058fc1a31c
Unknown <davidsourod@gmail.com>
2018-04-22 07:10:10 -0300

    Comments

--- parte1/spider.cpp
+++ parte1/spider.cpp
@@ -133,8 +133,8 @@ void mouse(GLint button, GLint action, GLint x, GLint y){
 			translationSteps = (int) norm(aux)+1;//calculo do numero de passos necessarios para a animacao de translacao da aranha. Pega-se a parte inteiro da norma do vetor auxiliar e soma-se 1
 			finalAngle = acos(dot(aux,orientation)/(norm(aux)*norm(orientation)));//calculo do angulo necessario para a animacao de rotacao da aranha. Calcula-se por meio do produto escalar
 			finalAngle = vecprod(aux,orientation)>0?-finalAngle:finalAngle;//calulo do sinal do angulo necessario para a animcao de rotacao da aranha. Calcula-se por meio do produto vetorial
-			glutTimerFunc(ANIMATION_SPEED,&rotate_spider,abs(finalAngle/ROTATE_STEP_SIZE));//chamada da funcao de animacao de rotacao da aranha
-			aux = orientation;//utilizas-se o vetor auxiliar para armazenar 
+			glutTimerFunc(ANIMATION_SPEED,&rotate_spider,abs(finalAngle/ROTATE_STEP_SIZE));//sinalizacao para a biblioteca GLUT que a funcao de animacao de rotacao da aranha deve ser chamada daqui a ANIMATIO_SPEED ms passando o ultimo parametro como argumento
+			aux = orientation;//reutilizacao do vetor auxiliar para rotacao do vetor orientacao
 			orientation.x = aux.x*cos(finalAngle) - aux.y*sin(finalAngle);//rotacao da coordenada x do vetor de orientacao
 			orientation.y = aux.x*sin(finalAngle) + aux.y*cos(finalAngle);//rotacao da coordenada y do vetor de orientacao
 			dest.x = x;///a coordenada x do ponto destino da aranha
@@ -210,7 +210,7 @@ void draw_legs(){
 void draw_body(){
 	for(int i = 0; i < NUMBER_OF_BODY_PARTS; i++){
 		glBegin(GL_LINE_LOOP);//inicializacao dos pontos a desenhar com GL_LINE_LOOP, cada ponto conecta-se ao anteriormente passado e o ultimo ponto passdad conecta-se com o primeiro passado
-			for(int j = 0; j < body[i].size(); j++)
+			for(int j = 0; j < CIRCLE_POINTS; j++)
 				glVertex2f(body[i][j].x, body[i][j].y);//passam-se os pontos das partes do corpo
 		glEnd();
 	}
@@ -218,148 +218,185 @@ void draw_body(){
 
 /**/
 //rotate_spider
-//funcao que faz a animacao do corpo da aranha
+//funcao que faz a animacao de rotacao do corpo da aranha
 //params:
 // - step: GLint, passo atual da animacao de rotacao do corpo da aranha
 /**/
 void rotate_spider(GLint step){
-	GLfloat rotate_step = finalAngle>0?ROTATE_STEP_SIZE:-ROTATE_STEP_SIZE;
-	GLfloat mat[9] = matRotate(rotate_step,center.x,center.y);
+	GLfloat rotate_step = finalAngle>0?ROTATE_STEP_SIZE:-ROTATE_STEP_SIZE;//verificacao da direcao da animacao de rotacao do corpo, se o angulo for negativo, sentido horario, e sentido anti-horario caso contrario
+	GLfloat mat[9] = matRotate(rotate_step,center.x,center.y);//matriz de rotacao em torno do centro da aranha
 	
-	GLfloat aux;
+	GLfloat aux;//variavel utilizada para auxiliar na multiplicacao de matrizes
 	for(int i = 0 ; i < NUMBER_OF_BODY_PARTS; i++)
 		for(int j = 0 ; j < CIRCLE_POINTS; j++){
-			aux = body[i][j].x;
-			body[i][j].x = aux*mat[0] + body[i][j].y*mat[3] + mat[6];
-			body[i][j].y = aux*mat[1] + body[i][j].y*mat[4] + mat[7];
+			aux = body[i][j].x;//multiplicao de matrizes: a matriz de rotacao vezes as coordenadas dos pontos das partes do corpo
+			body[i][j].x = aux*mat[0] + body[i][j].y*mat[3] + mat[6];//como utiliza-se coordenadas homogeneas a coordenada z nao eh multiplicada
+			body[i][j].y = aux*mat[1] + body[i][j].y*mat[4] + mat[7];//pois se mantera sempre igual e nao eh interrsessante fazer contas desnecessarias
 		}
 	for(int i = 0; i < NUMBER_OF_LEGS; i++)
 		for(int j = 0; j < NUMBER_OF_ARTICULATIONS+1; j++){
-			aux = legs[i].articulations[j].x;
-			legs[i].articulations[j].x = aux*mat[0] + legs[i].articulations[j].y*mat[3] + mat[6];
-			legs[i].articulations[j].y = aux*mat[1] + legs[i].articulations[j].y*mat[4] + mat[7];
+			aux = legs[i].articulations[j].x;//multiplicao de matrizes: a matriz de rotacao vezes as coordenadas dos pontos das pernas da aranha
+			legs[i].articulations[j].x = aux*mat[0] + legs[i].articulations[j].y*mat[3] + mat[6];//como utiliza-se coordenadas homogeneas a coordenada z nao eh multiplicada
+			legs[i].articulations[j].y = aux*mat[1] + legs[i].articulations[j].y*mat[4] + mat[7];//pois se mantera sempre igual e nao eh interrsessante fazer contas desnecessarias
 		}
-	move_legs();
-	glutPostRedisplay();
-	if( step > 0 )
-		glutTimerFunc(ANIMATION_SPEED,&rotate_spider,step-1);
-	else{
-		glutTimerFunc(ANIMATION_SPEED,&translate_spider,translationSteps);
-		center.x = dest.x;
-		center.y = dest.y;
+	move_legs();//funcao de animacao das pernas da aranha
+	glutPostRedisplay();//sinalizacao para a biblioteca GLUT para chamer a funcao que cuida do display
+	if( step > 0 )//se a animacao de rotacao da aranha ainda nao tiver acabado
+		glutTimerFunc(ANIMATION_SPEED,&rotate_spider,step-1);//sinaliza-se para a biblioteca GLUT que deve-se chamar a funcao novamente, mas com um passo a menos
+	else{//caso contrario
+		glutTimerFunc(ANIMATION_SPEED,&translate_spider,translationSteps);//se a animcao de rotacao acabou, sinaliza-se para a biblioteca GLUT que deve-se chmar a funcao de animcao de translacao da aranha daqui a ANIMATION_SPEED ms passando o ultimo parametro como argumento
+		center.x = dest.x;//translacao da coordenada x do centro da aranha
+		center.y = dest.y;//translacao da coordenada y do centro da aranha
 	}
 }
 
+/**/
+//translate_spider
+//funcao que faz animcao de translacao do corpo da aranha
+//params:
+// - step: GLint, passo atual da animacao de translacao do corpo da aranha
+/**/
 void translate_spider(GLint step){
-	GLfloat mat[9] = matTranslate(orientation.x,orientation.y);
+	GLfloat mat[9] = matTranslate(orientation.x,orientation.y);//matriz de translacao
 	
-	GLfloat aux;
+	GLfloat aux;//variavel utilizada para auxiliar na multiplicacao de matrizes
 	for(int i = 0 ; i < NUMBER_OF_BODY_PARTS; i++)
 		for(int j = 0 ; j < CIRCLE_POINTS; j++){
-			aux = body[i][j].x;
-			body[i][j].x = aux*mat[0] + body[i][j].y*mat[3] + mat[6];
-			body[i][j].y = aux*mat[1] + body[i][j].y*mat[4] + mat[7];
+			aux = body[i][j].x;//multiplicao de matrizes: a matriz de translacao vezes as coordenadas dos pontos das partes do corpo
+			body[i][j].x = aux*mat[0] + body[i][j].y*mat[3] + mat[6];//como utiliza-se coordenadas homogeneas a coordenada z nao eh multiplicada
+			body[i][j].y = aux*mat[1] + body[i][j].y*mat[4] + mat[7];//pois se mantera sempre igual e nao eh interrsessante fazer contas desnecessarias
 		}
 	for(int i = 0; i < NUMBER_OF_LEGS; i++)
 		for(int j = 0; j < NUMBER_OF_ARTICULATIONS+1; j++){
-			aux = legs[i].articulations[j].x;
-			legs[i].articulations[j].x = aux*mat[0] + legs[i].articulations[j].y*mat[3] + mat[6];
-			legs[i].articulations[j].y = aux*mat[1] + legs[i].articulations[j].y*mat[4] + mat[7];
+			aux = legs[i].articulations[j].x;//multiplicao de matrizes: a matriz de translacao vezes as coordenadas dos pontos das pernas da aranha
+			legs[i].articulations[j].x = aux*mat[0] + legs[i].articulations[j].y*mat[3] + mat[6];//como utiliza-se coordenadas homogeneas a coordenada z nao eh multiplicada
+			legs[i].articulations[j].y = aux*mat[1] + legs[i].articulations[j].y*mat[4] + mat[7];//pois se mantera sempre igual e nao eh interrsessante fazer contas desnecessarias
 		}
-	if( step > 0 ){
-		move_legs();
-		glutTimerFunc(ANIMATION_SPEED,&translate_spider,step-1);
-	}else{
-		restore_legs_position();
-		canWalk = true;
+	if( step > 0 ){//se a animacao de translacao da aranha ainda nao tiver acabado
+		move_legs();//a funcao de animcao das pernas da aranha eh chamada
+		glutTimerFunc(ANIMATION_SPEED,&translate_spider,step-1);//sinaliza-se para a biblioteca GLUT que deve-se chamar a funcao novamente, mas com um passo a menos
+	}else{//caso contrario
+		restore_legs_position();//chama-se a funcao que restaura a posicao das pernas a posicao que elas estriam se nao tivessem sido animadas
+		canWalk = true;//desbloquei-se a entrada de um novo ponto de destino para a aranha
 	}
-	glutPostRedisplay();
+	glutPostRedisplay();//sinalizacao para a biblioteca GLUT para chamer a funcao que cuida do display
 }
 
+/**/
+//move_legs
+//funcao de animcao das pernas da aranha
+/**/
 void move_legs(){
-	if(legsAnimationStep == LEGS_ANIMATION_STEPS){
-		legsAnimationSpeed *= -1;
-		invertTransformation = true;
-	}else if(legsAnimationStep == -LEGS_ANIMATION_STEPS){
-		legsAnimationSpeed *= -1;
-		invertTransformation = true;
-	}else if(legsAnimationStep == 0){
-		if(legsAnimationOption  == 1)
-			legsAnimationOption  = 2;
-		else
-			legsAnimationOption  = 1;
-		invertTransformation = false;
+	if(legsAnimationStep == LEGS_ANIMATION_STEPS){//se o passo da animacao das pernas da aranha for igual a LEGS_ANIMATION_STEPS
+		legsAnimationSpeed = -1;//a velocidade de animacao passa a ser -1
+		invertTransformation = true;//a transformacao de animacao das pernas da aranha passa ser a inversa
+	}else if(legsAnimationStep == -LEGS_ANIMATION_STEPS){//se o passo da animacao das pernas da aranha for igual a -LEGS_ANIMATION_STEPS
+		legsAnimationSpeed = 1;//a velocidade de animacao passa a ser 1
+		invertTransformation = true;//a transformacao de animacao das pernas da aranha passa ser a inversa
+	}else if(legsAnimationStep == 0){//se o passo da animcao das pernas da ranha for ihual a 0
+		if(legsAnimationOption  == 1)//se a animacao anterior for a 1
+			legsAnimationOption  = 2;//muda-se para a animacao 2
+		else//se a animacao anterior for a 2
+			legsAnimationOption  = 1;//muda-se para a animacao 1
+		invertTransformation = false;//a tranformacao de animacao das pernas da aranha deixa de ser a inversa
 	}
-	legsAnimationStep += legsAnimationSpeed;
-	for(int i = 0; i < NUMBER_OF_LEGS; i++)
+	legsAnimationStep += legsAnimationSpeed;//o passo da animacao das pernas da aranha eh incrementado com legsAnimationSpeed
+	for(int i = 0; i < NUMBER_OF_LEGS; i++)//para cada perna da aranha chama-se a funcao que anima uma perna da aranha
 		move_leg(CEPHALOTHORAX[legs[i].attachment].x,CEPHALOTHORAX[legs[i].attachment].y,0,i, invertTransformation, legsAnimationOption );
 }
 
+/**/
+//move_leg
+//funcao recursiva que anima uma perna da aranha
+//params:
+// - x: GLfloat, coordenada x do ponto fixo na rotacao e escala
+// - y: GLfloat, coordenada y do ponto fixo na rotacao e escala
+// - step: int, passo da animacao, articulacao atual da perna da aranha que se esta animando
+// - current_leg: int, perna atula que se esta animando
+// - invert: bool, inversacao ou nao da animacao da perna da aranha
+// - option: int, opcao de animacao da perna da aranha
+/**/
 void move_leg(GLfloat x, GLfloat y, int step, int current_leg,bool invert, int option){
-	if(step == NUMBER_OF_ARTICULATIONS+1)
+	if(step == NUMBER_OF_ARTICULATIONS+1)//condicao de parada da funcao recursiva
 		return;
-	GLfloat rotation;
-	GLfloat scale;
+	GLfloat rotation;//variavel auxiliar para inicializacao da matriz de transformacao
+	GLfloat scale;//variavel auxiliar para inicializacao da matriz de transformacao
 	switch(option){
-		case 1:
+		case 1://caso seja a animacao 1, obtem-se os valores dessa animacao para a perna atual a partir das matrizes correspondentes, e inverte-se a transformcao se necessario
 			rotation = invert?-legs_rotation1[step][current_leg]:legs_rotation1[step][current_leg];
 			scale = invert?1.0/legs_scale1[step][current_leg]:legs_scale1[step][current_leg];
 		break;
-		case 2:
+		case 2://caso seja a animacao 2, obtem-se os valores dessa animacao para a perna atual a partir das matrizes correspondentes, e inverte-se a transformcao se necessario
 			rotation = invert?-legs_rotation2[step][current_leg]:legs_rotation2[step][current_leg];
 			scale = invert?1.0/legs_scale2[step][current_leg]:legs_scale2[step][current_leg];
 		break;
 	}
-	GLfloat mat[9] = matRotateAndScale(rotation, x, y, scale);
-	GLfloat aux;
+	GLfloat mat[9] = matRotateAndScale(rotation, x, y, scale);//matriz que eh concatencao de 4 transformacao que foram feitas nessa ordem: translacao para a origem, rotacao, escala, translacao para as coordenadas originais do ponto fixo
 
+	GLfloat aux;//variavel utilizada para auxiliar na multiplicacao de matrizes
 	for(int j = step; j < NUMBER_OF_ARTICULATIONS+1; j++){
-		aux = legs[current_leg].articulations[j].x;
-		legs[current_leg].articulations[j].x = aux*mat[0] + legs[current_leg].articulations[j].y*mat[3] + mat[6];
-		legs[current_leg].articulations[j].y = aux*mat[1] + legs[current_leg].articulations[j].y*mat[4] + mat[7];
+		aux = legs[current_leg].articulations[j].x;//multiplicao de matrizes: a matriz de transformacao vezes as coordenadas dos pontos da perna da aranha que sao afetados por essa transformacao
+		legs[current_leg].articulations[j].x = aux*mat[0] + legs[current_leg].articulations[j].y*mat[3] + mat[6];//como utiliza-se coordenadas homogeneas a coordenada z nao eh multiplicada
+		legs[current_leg].articulations[j].y = aux*mat[1] + legs[current_leg].articulations[j].y*mat[4] + mat[7];//pois se mantera sempre igual e nao eh interrsessante fazer contas desnecessarias
 	}
 
-	move_leg(legs[current_leg].articulations[step].x,legs[current_leg].articulations[step].y,step+1,current_leg, invert, option);
+	move_leg(legs[current_leg].articulations[step].x,legs[current_leg].articulations[step].y,step+1,current_leg, invert, option);//chama-se recursivamente a funcao novamente somando-se um passo a mais 
 }
 
+/**/
+//restor_legs_position
+//funcao que restaura a posicao das pernas da aranha
+/**/
 void restore_legs_position(){
-	if(legsAnimationStep != 0){
-		if((legsAnimationSpeed > 0 && legsAnimationStep > 0 ) || (legsAnimationSpeed < 0 && legsAnimationStep < 0))
+	if(legsAnimationStep != 0){//se as pernas ja nao estiverem na posicao correta
+		if((legsAnimationSpeed > 0 && legsAnimationStep > 0 ) || (legsAnimationSpeed < 0 && legsAnimationStep < 0))//se necessario inverter a transformcao para restaurar a posicao das pernas da aranha
 			invertTransformation = !invertTransformation;
-		for(int i = 0; i < NUMBER_OF_LEGS; i++)
+		for(int i = 0; i < NUMBER_OF_LEGS; i++)//chama-se a funcao que restaura a posicao de uma perna da aranha para cada perna da aranha
 			restore_leg_position(CEPHALOTHORAX[legs[i].attachment].x,CEPHALOTHORAX[legs[i].attachment].y,0,i, invertTransformation, legsAnimationOption ,abs(legsAnimationStep));
 	}
-	legsAnimationOption  = 1;
-	invertTransformation = false;
-	legsAnimationStep = 0;
-	legsAnimationSpeed = 1;
+	legsAnimationOption  = 1;//defini-se a opcao de animacao das pernas da aranha como a 1
+	invertTransformation = false;//a inversao da animcao das pernas da aranha eh cancelada
+	legsAnimationStep = 0;//o passa da animcao das pernas da aranha eh reinicializado
+	legsAnimationSpeed = 1;//a velocidade da animcao das pernas da aranha eh reinicializada
 }
 
+/**/
+//restore_leg_position
+//funcao recursiva que restaura a posicao de uma perna da aranha
+//params:
+// - x: GLfloat, coordenada x do ponto fixo na rotacao e escala
+// - y: GLfloat, coordenada y do ponto fixo na rotacao e escala
+// - step: int, passo da animacao, articulacao atual da perna da aranha que se esta animando
+// - current_leg: int, perna atula que se esta animando
+// - invert: bool, inversacao ou nao da animacao da perna da aranha
+// - option: int, opcao de animacao da perna da aranha
+// - multipleSteps: int, numero de passos que seriam necessarios para restaurar a posicao das pernas se fosse utilizada a funcao de animacao das pernas da aranha
+/**/
 void restore_leg_position(GLfloat x, GLfloat y, int step, int current_leg,bool invert, int option, int multipleSteps){
-	if(step == NUMBER_OF_ARTICULATIONS+1)
+	if(step == NUMBER_OF_ARTICULATIONS+1)//condicao de parada da funcao recursiva
 		return;
-	GLfloat rotation;
-	GLfloat scale;
+	GLfloat rotation;//variavel auxiliar para inicializacao da matriz de transformacao
+	GLfloat scale;//variavel auxiliar para inicializacao da matriz de transformacao
 	switch(option){
-		case 1:
+		case 1://caso seja a animacao 1, obtem-se os valores dessa animacao para a perna atual a partir das matrizes correspondentes, e inverte-se a transformcao se necessario
 			rotation = invert?-legs_rotation1[step][current_leg]:legs_rotation1[step][current_leg];
 			scale = invert?1.0/legs_scale1[step][current_leg]:legs_scale1[step][current_leg];
 		break;
-		case 2:
+		case 2://caso seja a animacao 2, obtem-se os valores dessa animacao para a perna atual a partir das matrizes correspondentes, e inverte-se a transformcao se necessario
 			rotation = invert?-legs_rotation2[step][current_leg]:legs_rotation2[step][current_leg];
 			scale = invert?1.0/legs_scale2[step][current_leg]:legs_scale2[step][current_leg];
 		break;
 	}
-	rotation *= multipleSteps;
-	scale = pow(scale,(float) multipleSteps);
-	GLfloat mat[9] = matRotateAndScale(rotation, x, y, scale);
-	GLfloat aux;
+	rotation *= multipleSteps;//multiplica-se o angulo para realizar uma transformcao apenas ao inves de varias pequenas
+	scale = pow(scale,(float) multipleSteps);//eleva-se a escala para realizar uma transformcao apenas ao inves de varias pequenas
+	GLfloat mat[9] = matRotateAndScale(rotation, x, y, scale);//matriz que eh concatencao de 4 transformacao que foram feitas nessa ordem: translacao para a origem, rotacao, escala, translacao para as coordenadas originais do ponto fixo
 
+	GLfloat aux;//variavel utilizada para auxiliar na multiplicacao de matrizes
 	for(int j = step; j < NUMBER_OF_ARTICULATIONS+1; j++){
-		aux = legs[current_leg].articulations[j].x;
-		legs[current_leg].articulations[j].x = aux*mat[0] + legs[current_leg].articulations[j].y*mat[3] + mat[6];
-		legs[current_leg].articulations[j].y = aux*mat[1] + legs[current_leg].articulations[j].y*mat[4] + mat[7];
+		aux = legs[current_leg].articulations[j].x;//multiplicao de matrizes: a matriz de transformacao vezes as coordenadas dos pontos da perna da aranha que sao afetados por essa transformacao
+		legs[current_leg].articulations[j].x = aux*mat[0] + legs[current_leg].articulations[j].y*mat[3] + mat[6];//como utiliza-se coordenadas homogeneas a coordenada z nao eh multiplicada
+		legs[current_leg].articulations[j].y = aux*mat[1] + legs[current_leg].articulations[j].y*mat[4] + mat[7];//pois se mantera sempre igual e nao eh interrsessante fazer contas desnecessarias
 	}
 
-	restore_leg_position(legs[current_leg].articulations[step].x,legs[current_leg].articulations[step].y,step+1,current_leg, invert, option, multipleSteps);
+	restore_leg_position(legs[current_leg].articulations[step].x,legs[current_leg].articulations[step].y,step+1,current_leg, invert, option, multipleSteps);//chama-se recursivamente a funcao novamente somando-se um passo a mais 
 }
